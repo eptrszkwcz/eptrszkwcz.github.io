9864e81b002e9857b56cace84c129d74fa0c0d49
eptrszkwcz <erp2151@columbia.edu>
2018-02-21 11:28:51 -0500

    adding MH plot

--- js/4A/sketch.js
+++ js/4A/sketch.js
@@ -1,12 +1,11 @@
 function setup(){
   var width = 540
   var height = 540
-  createCanvas(width,height);
+  createCanvas(540,540);
   colorMode(HSB,360,100,100,1)
 }
 
 function draw(){
-  // rect(width/2,height/2,30,30)
   for (var x=0; x<36; x++){
       for (var y=0; y<36; y++){
         // strokeWeight(0)
