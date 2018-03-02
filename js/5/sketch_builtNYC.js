8e9151a32f881e9c8396d23fb066252ee24f1213
eptrszkwcz <erp2151@columbia.edu>
2018-03-01 22:28:55 -0500

    ass5 changes?

--- js/5/sketch_builtNYC.js
+++ js/5/sketch_builtNYC.js
@@ -5,36 +5,32 @@ var coordinate =[]
 var builtArea = []
 var potential = []
 var landuse = []
+var year = []
+var selectedButton = 1
 
 
 function preload(){
   var filePath = 'data/MN_Pluto_Scatter.geojson'
   PlutoData = loadJSON(filePath);
-  // console.log('shit is lock n loaded!!');
+  console.log('dat shit is lock n loaded!!');
 }
 
 function setup(){
-  createCanvas(1000,1000);
-  background(255,255,255);
+  createCanvas(980,905);
   noLoop();
 }
 
-
 function draw(){
+  background(255)
+  stroke(0)
+  noFill()
+  strokeWeight(1)
+  rect(width/40,80,800,800);
+  strokeWeight(1)
+  noStroke();
+  draw_labels();
+  legend_landUse()
 
-  textAlign(LEFT)
-  textSize(30)
-  textStyle(BOLD)
-  text("BUILT NYC",width/40, 40)
-  textStyle(ITALIC)
-  textSize(14)
-  text("VISUALIZING UNDERBUILT AND OVERBUILT LOTS IN MANHATTAN", width/40, 60)
-
-  textAlign(CENTER)
-  textStyle(NORMAL)
-  textSize(11)
-  text("OVERBUILT", width/4,height/4)
-  text("UNDERBUILT", width*(2/3), height*(2/3))
 
   for (var i=0; i<38983; i++){
 
@@ -42,6 +38,8 @@ function draw(){
     potential = PlutoData.features[i].properties.Potential
     coordinates = PlutoData.features[i].geometry.coordinates
     landuse = PlutoData.features[i].properties.LandUse
+    year = PlutoData.features[i].properties.YearBuilt
+    block = PlutoData.features[i].properties.Block
 
     var x_value = log(potential)*78
     if (parseInt(builtArea)===0){
@@ -49,51 +47,181 @@ function draw(){
     } else{
       var y_value = log(builtArea)*78
     }
+      draw_FillLandUse()
 
 
-    noStroke()
-    if (parseInt(landuse) === 1){
-      fill(255,254,85)
-      draw_shape(coordinates)
-    } if (parseInt(landuse) === 2){
-      fill(255,254,85)
-      draw_shape(coordinates)
-    } if (parseInt(landuse) === 3){
-      fill(255,254,85)
-      draw_shape(coordinates)
-    } if (landuse === 4){
-      fill(232,87,0)
-      draw_shape(coordinates)
-    } if (parseInt(landuse) === 5){
-      fill(255,0,0)
-      draw_shape(coordinates)
-    } if (parseInt(landuse) === 6){
-      fill(116,0,232)
-      draw_shape(coordinates)
-    } else {
-      fill(255,255,255)
-    }
-    stroke(0,0,0)
-    line(0,height-95, width-95,0)
-
+    function draw_shape(points){
+      beginShape()
+      for (var j=0; j<points[0][0].length; j++){
+        var x_anchor = points[0][0][0][0]
+        var y_anchor = points[0][0][0][1]
+        var delta_x = x_value-(parseInt((x_anchor-936000)/70))
+        var delta_y = y_value-(parseInt((y_anchor-162000)/70))
+        var x = points[0][0][j][0]
+        var x_mod = (parseInt((x-975000)/70))+delta_x
+        // console.log(parseInt(x_mod))
+        var y = points[0][0][j][1]
+        var y_mod = ((parseInt((y-192000)/70)))+delta_y
+        // console.log(y_mod)
+        vertex(x_mod, 960-y_mod)
+      }
+      endShape(CLOSE);
     }
 
-function draw_shape (points){
-  beginShape()
-  for (var j=0; j<points[0][0].length; j++){
-    var x_anchor = points[0][0][0][0]
-    var y_anchor = points[0][0][0][1]
-    var delta_x = x_value-(parseInt((x_anchor-940000)/70))
-    var delta_y = y_value-(parseInt((y_anchor-165000)/70))
-    var x = points[0][0][j][0]
-    var x_mod = (parseInt((x-975000)/70))+delta_x
-    // console.log(parseInt(x_mod))
-    var y = points[0][0][j][1]
-    var y_mod = ((parseInt((y-192000)/70)))+delta_y
-    // console.log(y_mod)
-    vertex(x_mod, height-y_mod)
+    function draw_FillLandUse(){
+      noStroke()
+      if (parseInt(landuse) === 1){
+        fill(255,254,85)
+        draw_shape(coordinates)
+      } if (parseInt(landuse) === 2){
+        fill(255,254,85)
+        draw_shape(coordinates)
+      } if (parseInt(landuse) === 3){
+        fill(255,254,85)
+        draw_shape(coordinates)
+      } if (landuse === 4){
+        fill(255,156,0)
+        draw_shape(coordinates)
+      } if (parseInt(landuse) === 5){
+        fill(255,0,0)
+        draw_shape(coordinates)
+      } if (parseInt(landuse) === 6){
+        fill(116,0,232)
+        draw_shape(coordinates)
+      } else {
+        fill(255,255,255)
+      }
+    }
+
   }
-  endShape(CLOSE);
+    stroke(0)
+    strokeWeight(1.25)
+    line(width/40,820, (width/40)+720,81)
+    strokeWeight(1)
+  }
+
+function draw_labels(){
+  fill(0)
+  textAlign(LEFT, BOTTOM)
+  textSize(30)
+  textStyle(BOLD)
+  text("BUILT NYC",width/40, 40)
+  textStyle(ITALIC)
+  textSize(14)
+  text("VISUALIZING UNDERBUILT AND OVERBUILT LOTS IN MANHATTAN BY...", width/40, 60)
+
+  textStyle(BOLD)
+  textAlign(LEFT, TOP)
+  textSize(11)
+  text("OVERBUILT", 40, 90)
+  textAlign(RIGHT, TOP)
+  text("UNDERBUILT", 815, 855)
+  textAlign(LEFT, BOTTOM)
+  textStyle(NORMAL)
 }
 
+function button(){
+
+  noStroke();
+  textStyle(ITALIC)
+  textAlign(LEFT)
+  textSize(14)
+  if (selectedButton == 0){
+    fill(0);
+    text("LAND USE", 525, 60)
+    fill(220)
+    text("BUILDING AGE", 620, 60)
+    text("LATITUDE", 745, 60)
+  } if (selectedButton == 1){
+    fill(0);
+    text("BUILDING AGE", 620, 60)
+    fill(220)
+    text("LAND USE", 525, 60)
+    text("LATITUDE", 745, 60)
+  } if (selectedButton == 2){
+    fill(0);
+    text("LATITUDE", 745, 60)
+    fill(220)
+    text("LAND USE", 525, 60)
+    text("BUILDING AGE", 620, 60)
+  }
+
+}
+
+function legend_landUse(){
+  textAlign(LEFT, TOP)
+  textStyle(NORMAL)
+  textSize(12)
+  for (var i=0; i<5; i++){
+    if (i==0){
+      fill(0)
+      text("Residential", (width/40)+845, 81+(20*i))
+      fill(255,254,85)
+    } else if (i==1){
+      fill(0)
+      text("Mixed Use", (width/40)+845, 81+(20*i))
+      fill(255,156,0)
+    } else if (i==2){
+      fill(0)
+      text("Commercial", (width/40)+845, 81+(20*i))
+      fill(255,0,0)
+    } else if (i==3){
+      fill(0)
+      text("Industrial", (width/40)+845, 81+(20*i))
+      fill(116,0,232)
+    } else if (i==4){
+      fill(0)
+      text("Vacant", (width/40)+845, 81+(20*i))
+      fill(50)
     }
+    rect((width/40)+820, 80+(20*i), 15, 15)
+    fill(0)
+  }
+}
+
+function setGradient1(x, y, w, h){
+  for (var i = 0; i < h; i++) {
+    fill(255-(i*2.684), i*2.864, 232-(i*0.536))
+    rect(x,y+i, w,1)
+
+   }
+}
+
+function setGradient2(x, y, w, h){
+  for (var i = 0; i < h; i++) {
+    fill(0+(i*2.684), 255, 255-(i*2.684))
+    rect(x,y+i, w,1)
+   }
+}
+
+function legend_year(){
+  fill(0)
+  textAlign(LEFT, TOP)
+  textStyle(NORMAL)
+  textSize(12)
+  text("200+ years", (width/40)+845, 81)
+  text("0 years", (width/40)+845, 161)
+  setGradient1((width/40)+820, 80, 15, 95)
+}
+
+function legend_lat(){
+  fill(0)
+  textAlign(LEFT, TOP)
+  textStyle(NORMAL)
+  textSize(12)
+  text("N (Innwood)", (width/40)+845, 81)
+  text("S (South Ferry)", (width/40)+845, 161)
+  setGradient2((width/40)+820, 80, 15, 95)
+}
+
+function mousePressed(){
+  if (mouseX>510&&mouseX<590&&mouseY<60&&mouseY>46){
+    selectedButton=0
+    redraw();
+  } else if (mouseX>610&&mouseX<720&&mouseY<60&&mouseY>46){
+    selectedButton=1
+    redraw();
+  } else if (mouseX>740&&mouseX<820&&mouseY<60&&mouseY>46){
+    selectedButton=2
+    redraw();
+}}
